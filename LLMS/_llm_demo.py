5d8dfae9b0ac180e3a7e5c8f09846f5398ec4fe6
Sumiitt-007 <sumitrpt007@gmail.com>
2025-08-21 23:57:06 +0530

    Fetched the first few responses and started with the GenAI

---
+++ LLMS/_llm_demo.py
@@ -0,0 +1,17 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+api_key = os.getenv("GOOGLE_API_KEY")
+
+llm = ChatGoogleGenerativeAI(
+  model="gemini-1.5-flash",
+  google_api_key=api_key
+)
+result = llm.invoke(
+  "Which is the best anime?"
+)
+
+print(result.content)
